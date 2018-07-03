359c02b7738d931b1495fd16cea1a7000732eac1
teodosii <teodosii3@outlook.com>
2018-07-03 11:36:20 +0300

    Changed App.js to use custom types inline

--- samples/js/components/App.js
+++ samples/js/components/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactNotification from "react-notifications-component";
 import Header from "./Header";
 import Content from "./Content";
-import { types } from "../helpers/custom";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -11,14 +10,17 @@ export default class App extends React.Component {
   }
 
   addNotification(notification) {
-    this.instance.addNotification(notification);
+    return this.instance.addNotification(notification);
   }
 
   render() {
     return (
       <React.Fragment>
         <ReactNotification
-          types={types}
+          types={[{
+            htmlClasses: ["notification-awesome"],
+            name: "awesome"
+          }]}
           ref={input => this.instance = input}
           isMobile={true}
         />
--- samples/js/helpers/custom.js
+++
@@ -1,4 +0,0 @@
-export const types = [{
-  htmlClasses: ["notification-awesome"],
-  name: "awesome"
-}];
