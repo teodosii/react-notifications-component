d817ecdd5de207b3c694d793366dde4ba2e2b11d
teodosii <teodosii3@outlook.com>
2018-07-21 00:10:14 +0300

    Updated errors to be more descriptive

--- src/errors.js
+++ src/errors.js
@@ -1,51 +1,50 @@
 export default {
   // dismiss icon option
-  DISMISS_ICON_CLASS: "`className` property of `dismissIcon` is required",
-  DISMISS_ICON_CONTENT: "`content` property of `dismissIcon` is required",
-  DISMISS_ICON_STRING: "`className` property of `dismissIcon` must be a String",
-  DISMISS_ICON_INVALID: "`content` property of `dismissIcon` must be a valid React element",
+  DISMISS_ICON_CLASS: "className property of dismissIcon option is required",
+  DISMISS_ICON_CONTENT: "content property of dismissIcon option is required",
+  DISMISS_ICON_STRING: "className property of dismissIcon option must be a String",
+  DISMISS_ICON_INVALID: "content property of dismissIcon option must be a valid React element",
 
   // animations
-  ANIMATION_IN: "`animationIn' must be an array",
-  ANIMATION_OUT: "`animationOut' must be an array",
+  ANIMATION_IN: "animationIn option must be an array",
+  ANIMATION_OUT: "animationOut option must be an array",
 
   // dismiss
-  DISMISS_REQUIRED: "Dismiss `duration` is required",
-  DISMISS_NUMBER: "Dismiss `duration` must be a Number",
-  DISMISS_POSITIVE: "Dismiss `duration` must be a positive Number",
+  DISMISS_REQUIRED: "duration property of dismiss option is required",
+  DISMISS_NUMBER: "duration property of dismiss option must be a Number",
+  DISMISS_POSITIVE: "duration property of dismiss option must be a positive Number",
 
   // title
-  TITLE_STRING: "`title` must be a String.",
-  TITLE_REQUIRED: "`title` is required",
+  TITLE_STRING: "title option must be a String.",
 
   // message
-  MESSAGE_REQUIRED: "`message` is required",
-  MESSAGE_STRING: "`message` must be a String",
+  MESSAGE_REQUIRED: "message option is required",
+  MESSAGE_STRING: "message option must be a String",
 
   // type
-  TYPE_REQUIRED: "`type` is required",
-  TYPE_STRING: "`type` must be a String",
-  TYPE_NOT_EXISTENT: "`type` could not be found in existing types. Typo maybe?",
+  TYPE_REQUIRED: "type option is required",
+  TYPE_STRING: "type option must be a String",
+  TYPE_NOT_EXISTENT: "type option not found",
 
   // container
-  CONTAINER_REQUIRED: "`container` is required",
-  CONTAINER_STRING: "`container` must be a String",
+  CONTAINER_REQUIRED: "container option is required",
+  CONTAINER_STRING: "container option must be a String",
 
   // dismissable
-  DISMISSABLE_CLICK_BOOL: "`click` property from `dismissable` must be a Boolean",
-  DISMISSABLE_TOUCH_BOOL: "`touch` property from `dismissable` must be a Boolean",
+  DISMISSABLE_CLICK_BOOL: "click property of dismissable option must be a Boolean",
+  DISMISSABLE_TOUCH_BOOL: "touch property of dismissable option must be a Boolean",
 
   // width
-  WIDTH_NUMBER: "`width` must be a Number",
+  WIDTH_NUMBER: "width option must be a Number",
 
   // insert
-  INSERT_STRING: "`insert` must be a String",
+  INSERT_STRING: "insert option must be a String",
 
   // transition
-  TRANSITION_DURATION_NUMBER: "`duration` property of transition must be a Number",
-  TRANSITION_CUBICBEZIER_NUMBER: "`cubicBezier` property of transition must be a String",
-  TRANSITION_DELAY_NUMBER: "`delay` property of transition must be a Number",
+  TRANSITION_DURATION_NUMBER: "duration property of transition option must be a Number",
+  TRANSITION_CUBICBEZIER_NUMBER: "cubicBezier property of transition option must be a String",
+  TRANSITION_DELAY_NUMBER: "delay property of transition option must be a Number",
 
   // custom types
-  TYPE_NOT_FOUND: "Custom type not found. Typo maybe?"
+  TYPE_NOT_FOUND: "custom type not found"
 };
