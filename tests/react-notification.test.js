6cfca1241455d447ffd569362afb38e2ec492c4c
teodosii <teodosii3@outlook.com>
2018-08-26 14:06:34 +0300

    Updated react-notification.test.js for better coverage

--- tests/react-notification.test.js
+++ tests/react-notification.test.js
@@ -18,6 +18,7 @@ describe("Notification component", () => {
   // arrow function helpers
   const getNotificationMock = (edits = {}) => Object.assign({}, notificationMock, edits);
   const instance = () => component.instance();
+  const state = () => component.state();
 
   beforeEach(() => {
     // set fake timer
@@ -32,6 +33,114 @@ describe("Notification component", () => {
       // unmount
       component.unmount();
     }
+  });
+
+  it("onTransitionEnd updates state with needed CSS classes", () => {
+    component = mount(
+      <ReactNotification notification={getNotificationMock({ animationIn: ["html5", "css3"] })} />
+    );
+
+    // manually call function
+    component.instance().onTransitionEnd();
+    // expect correct state update
+    expect(component.state).toMatchSnapshot();
+
+    component = mount(
+      <ReactNotification notification={getNotificationMock({ animationIn: null })} />
+    );
+    // manually call function
+    component.instance().onTransitionEnd();
+    // expect correct state update
+    expect(component.state).toMatchSnapshot();
+  });
+
+  it("onTouchSmartSlidingEnd returns if target node is NOT the root node", () => {
+    component = mount(
+      <ReactNotification notification={getNotificationMock({
+        stage: NOTIFICATION_STAGE.TOUCH_SLIDING_ANIMATION_EXIT
+      })} />
+    );
+
+    component.find(".notification-item-child").simulate("transitionend");
+    // expect endOfSmartSliding to be false
+    expect(instance().endOfSmartSliding).toBe(false);
+  });
+
+  it("onTouchSmartSlidingEnd toggles removal", () => {
+    const toggleRemoval = jest.fn();
+    component = mount(
+      <ReactNotification toggleRemoval={toggleRemoval} notification={getNotificationMock({
+        stage: NOTIFICATION_STAGE.TOUCH_SLIDING_ANIMATION_EXIT
+      })} />
+    );
+
+    component.find(".notification-item-root").simulate("transitionend")
+    // expect endOfSmartSliding to be true
+    expect(instance().endOfSmartSliding).toBe(true);
+
+    component.find(".notification-item-root").simulate("transitionend")
+    // expect toggleRemoval to be called
+    expect(toggleRemoval.mock.calls.length).toBe(1);
+  });
+
+  it("onSmartSlidingEnd toggles removal for undefined/empty animationOut", () => {
+    const toggleRemoval = jest.fn();
+    component = mount(
+      <ReactNotification toggleRemoval={toggleRemoval} notification={getNotificationMock({
+        stage: NOTIFICATION_STAGE.SLIDING_ANIMATION_EXIT,
+        animationOut: null
+      })} />
+    );
+
+    // trigger transitionend event
+    component.find(".notification-item-root").simulate("transitionend");
+    expect(toggleRemoval.mock.calls.length).toBe(1);
+
+    component = mount(
+      <ReactNotification toggleRemoval={toggleRemoval} notification={getNotificationMock({
+        stage: NOTIFICATION_STAGE.SLIDING_ANIMATION_EXIT,
+        animationOut: []
+      })} />
+    );
+
+    // trigger transitionend event
+    component.find(".notification-item-root").simulate("transitionend");
+    expect(toggleRemoval.mock.calls.length).toBe(2);
+  });
+
+  it("onSmartSlidingEnd toggles removal on 2nd function call", () => {
+    const toggleRemoval = jest.fn();
+    component = mount(
+      <ReactNotification toggleRemoval={toggleRemoval} notification={getNotificationMock({
+        stage: NOTIFICATION_STAGE.SLIDING_ANIMATION_EXIT
+      })} />
+    );
+
+    // trigger transitionend event
+    component.find(".notification-item-root").simulate("transitionend");
+    expect(toggleRemoval.mock.calls.length).toBe(0);
+
+    // trigger transitionend event
+    component.find(".notification-item-root").simulate("transitionend");
+    expect(toggleRemoval.mock.calls.length).toBe(1);
+  });
+
+  it("componentDidMount sets initial state", () => {
+    const spy = jest.spyOn(ReactNotification.prototype, "componentDidMount");
+
+    component = mount(
+      <ReactNotification notification={getNotificationMock({ resized: false })} />
+    );
+
+    expect(spy.mock.calls.length).toBe(1);
+    expect(state()).toMatchSnapshot();
+
+    component = mount(
+      <ReactNotification notification={getNotificationMock({ resized: true })} />
+    );
+
+    expect(spy.mock.calls.length).toBe(2);
+    expect(state()).toMatchSnapshot();
   });
 
   it("timeout handler skips setting state if stage is REMOVAL/EXIT", () => {
@@ -106,8 +215,8 @@ describe("Notification component", () => {
   });
 
   it("clicks notification and calls onNotificationClick prop", () => {
-    let onClickHandler = jest.fn();
-    let spy = jest.spyOn(ReactNotification.prototype, "onNotificationClick");
+    const onClickHandler = jest.fn();
+    const spy = jest.spyOn(ReactNotification.prototype, "onNotificationClick");
 
     component = mount(<ReactNotification
       notification={notificationMock}
